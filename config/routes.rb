caecf8395c20d283499fd8eb1583b57d00ba71f9
Takashi Furuya <takfuruya@gmail.com>
2013-05-14 12:30:52 -0400

    Fix signup broken link

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ SampleApp::Application.routes.draw do
 
   root              to: 'static_pages#home'
   
+  match '/signup',  to: 'users#new'
+
   match '/help',    to: 'static_pages#help'
   
   match '/about',   to: 'static_pages#about'
   
   match '/contact', to: 'static_pages#contact'
 
-  match '/signup',  to: 'users#new'
+  
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
